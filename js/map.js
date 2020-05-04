3c7fafdf72f634ef28d926422c02958f685f324d
Danielle Hatten <dhatte2@uic.edu>
2020-05-04 19:39:54 +0000

    map added using google maps api

--- js/map.js
+++ js/map.js
@@ -1,57 +1,58 @@
-//function initMap() {
-let node_location = {lat: 41.8781136, lng: -87.6297982}
 
-/*var map = new google.maps.Map(document.getElementById('map'), {
-  zoom: 6,
-  center: node_location
-});*/
 
-db.PM25.toArray()
-  .then((array) => {
-    for (a of array){
-      console.log(a);
-    }
 
-  })
-
-/*for (let i of inspection_data) {
-
-  let res = (i.inspection_date).split("T"); 
-  let contentString =  '<div id="content">' + 
-    '<h3 class = "firstHeading">' +
-    i.dba_name +
-    '</h3>' +
-    '<p>' +
-    i.facility_type +
-    '</p>' +
-    '<p>' +
-    i.address +
-    '</p>' +
-    '<p>' +
-    res[0] +
-    '</p>' +
-    '<p>' +
-    i.results +
-    '</p>' +
-
-    '</div>';
+let chart_button = document.querySelector('.chart-button');
+  
+chart_button.addEventListener('click', (e) => {
+    
+  // go to chart page once button is clicked
+  document.querySelector("#mychart").style.display = "block";
+  document.querySelector("#mydata").style.display = "none";
+  document.querySelector("#mytable").style.display = "none";
+    
+  
+  
+  //function initMap() {
+    let node_location = {lat: 41.8781136, lng: -87.6297982};
+    
+  let map = new google.maps.Map(document.getElementById('mychart'), {
+    zoom: 6,
+    center: node_location
+  });
 
 
   let infoWindow = new google.maps.InfoWindow({
-    content: contentString
-    //content: [i.dba_name]
+    content: 'here'
+    /*content: [i.dba_name]*/
   });
 
   let marker = new google.maps.Marker({
-    position: {lat: parseFloat(i["latitude"]), lng: parseFloat(i["longitude"])},
+    position: {lat: parseFloat(node_location["lat"]), lng: parseFloat(node_location["lng"])},
     map: map,
-    title: i["dba_name"]
+    title: 'here'
   })
 
   marker.addListener('click', function() {
     infoWindow.open(map, marker);
   });
-};*/
+          
+  //};
+  
+  
+
+  
+})
 
 
-//}
+
+
+
+
+
+
+
+
+
+
+
+
